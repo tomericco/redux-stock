e023fd38946f30e31920013e5b1f363e247a994c
tomericco <tomer@urbanist.io>
2016-03-28 10:58:23 +0300

    Clean code

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var app = new (require('express'))()
 app.set('port', process.env.PORT || 3000)
 
 var compiler = webpack(config)
+var isProduction = process.env.NODE_ENV === 'production'
 
-if (process.env.NODE_ENV !== 'production') {
+if (!isProduction) {
   var webpackDevMiddleware = require('webpack-dev-middleware')
   var webpackHotMiddleware = require('webpack-hot-middleware')
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
